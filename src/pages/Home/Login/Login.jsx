6d7d034cd58611d8a6b314e8477c37beeedf14d4
Toyir Tursunxojayev <tursunxojayevtoyir2@gmail.com>
2025-05-05 11:03:29 +0500

    oferta qo'shildi

--- src/pages/Home/Login/Login.jsx
+++ src/pages/Home/Login/Login.jsx
@@ -4,6 +4,7 @@ import "../Login/Login.css";
 import { baseUrl } from '../../../constants/base-url';
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
+// import dox from "../../../../public/oferta.docx"
 
 const Login = () => {
   const navigate = useNavigate();
@@ -49,35 +50,37 @@ const Login = () => {
     <div className='container-1'>
       <div className='login-form'>
         <div className="login-input">
-          <input 
-            onChange={onchangeNumber} 
-            value={phoneNumber} 
-            className='login-number' 
-            type="text" 
-            placeholder='Phone number' 
+          <input
+            onChange={onchangeNumber}
+            value={phoneNumber}
+            className='login-number'
+            type="text"
+            placeholder='Phone number'
           />
-          <input 
-            onChange={onchangePasword} 
-            value={password} 
-            className='login-password' 
-            type="password" 
-            placeholder='Password' 
+          <input
+            onChange={onchangePasword}
+            value={password}
+            className='login-password'
+            type="password"
+            placeholder='Password'
           />
-          
+
           {/* Checkbox qo'shildi */}
           <label className="checkbox-label">
-            <input 
-              type="checkbox" 
-              checked={isChecked} 
-              onChange={handleCheckboxChange} 
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={handleCheckboxChange}
               className='oferta'
             />
-             
-              Saytdan foydalanish shartlariga roziman
+
+            Saytdan foydalanish <a style={{color: "blue"}} href="/oferta.docx" target="_blank" rel="noopener noreferrer">
+             shartlariga  
+            </a> roziman
           </label>
-          
-          <button 
-            onClick={submitLogin} 
+
+          <button
+            onClick={submitLogin}
             className="input-submit relative hidden lg:inline-flex h-12 active:scale-95 transition overflow-hidden rounded-lg p-[2px] focus:outline-none"
           >
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#007f7f_0%,#009494_50%,#00b3b3_100%)]"></span>
